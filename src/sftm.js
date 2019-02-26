730de995a509adbc62160c6967dd7934a7dc10ce
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-02-26 10:56:42 -0500

    v1.0.0

--- src/sftm.js
+++ src/sftm.js
@@ -1,2 +1,26 @@
 // SFTM.js
 // Forces teacher mode in Seesaw on non-teacher accounts.
+alert("Welcome to SFTM! Please use sftm.help(), sftm.hook(), or sftm.switch(). If you don't know what to do, use sftm.help().")
+var sftm = new Object();
+sftm.help = function() {
+console.log("Welcome to SFTM! This programs lets you forcibly create a SeeSaw");
+console.log("teacher account forcibily. This program requires you to do a few");
+console.log("things that the script can't do. Thanks!");
+console.log("----");
+console.log("sftm.guide() - Launches the script in guide mode.");
+console.log("sftm.hook() - Creates a teacher account.");
+console.log("sftm.switch() - Switches to a teacher account.");
+console.log("sftm.why() - Benefits of a teacher account.");
+}
+sftm.guide = function() {
+alert("err: not implemented.")
+}
+sftm.hook = function() {
+alert("err: not implemented.")
+}
+sftm.switch = function() {
+alert("err: not implemented.")
+}
+sftm.why = function() {
+alert("err: not implemented.")
+}
